4f5645deecb78a0b350d5fd7636e0d2165f27d4c
Code Knightz <codeonbush@foxmail.com>
2019-08-15 09:40:54 +0800

    add

---
+++ [2]两数相加.java
@@ -0,0 +1,50 @@
+//给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。 
+//
+// 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。 
+//
+// 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 示例： 
+//
+// 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+//输出：7 -> 0 -> 8
+//原因：342 + 465 = 807
+// 
+//
+
+import java.util.Stack;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = buildStack(l1);
+        Stack<Integer> stack2 = buildStack(l2);
+        ListNode head = new ListNode();
+        int carry = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int x = stack1.isEmpty() ? 0 : stack1.pop();
+            int y = stack2.isEmpty() ? 0 : stack2.pop();
+            int sum = x + y +carry;
+            carry = sum % 10;
+            ListNode node = new ListNode(sum / 10);
+            node.next = head.next;
+            head.next = node;
+        }
+        return head;
+    }
+    private Stack<Integer> buildStack(ListNode l) {
+        Stack<Integer> stack = new Stack<>();
+        while (l != null) {
+            stack.push(l.val);
+            l = l.next;
+        }
+        return stack;
+    }
+}
