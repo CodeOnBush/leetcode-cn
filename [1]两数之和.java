0fa31fcb6fa0f04c4656ba4f8f36d18fe5219b6d
Code Knightz <codeonbush@foxmail.com>
2019-08-15 11:19:24 +0800

    add

--- [1]两数之和.java
+++ [1]两数之和.java
@@ -11,8 +11,21 @@
 // 
 //
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer, Integer> map = new HashMap<>(16);
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp) && map.get(temp) != i) {
+                return new int[] {i,map.get(temp)};
+            }
+        }
+        throw new IllegalArgumentException("gg");
     }
 }
--- [2]两数相加.java
+++ [2]两数相加.java
@@ -26,18 +26,18 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         Stack<Integer> stack1 = buildStack(l1);
         Stack<Integer> stack2 = buildStack(l2);
-        ListNode head = new ListNode();
+        ListNode head = new ListNode(-1);
         int carry = 0;
         while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
             int x = stack1.isEmpty() ? 0 : stack1.pop();
             int y = stack2.isEmpty() ? 0 : stack2.pop();
             int sum = x + y +carry;
-            carry = sum % 10;
-            ListNode node = new ListNode(sum / 10);
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
             node.next = head.next;
             head.next = node;
         }
-        return head;
+        return head.next;
     }
     private Stack<Integer> buildStack(ListNode l) {
         Stack<Integer> stack = new Stack<>();
