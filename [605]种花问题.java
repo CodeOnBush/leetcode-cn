f000b05910b082e498e3b031d7bd251cb5a3a990
Code Knightz <codeonbush@foxmail.com>
2019-09-17 17:50:42 +0800

    add

---
+++ [605]种花问题.java
@@ -0,0 +1,52 @@
+//假设你有一个很长的花坛，一部分地块种植了花，另一部分却没有。可是，花卉不能种植在相邻的地块上，它们会争夺水源，两者都会死去。 
+//
+// 给定一个花坛（表示为一个数组包含0和1，其中0表示没种植花，1表示种植了花），和一个数 n 。能否在不打破种植规则的情况下种入 n 朵花？能则返回True，不能则返回False。 
+//
+// 示例 1: 
+//
+// 
+//输入: flowerbed = [1,0,0,0,1], n = 1
+//输出: True
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: flowerbed = [1,0,0,0,1], n = 2
+//输出: False
+// 
+//
+// 注意: 
+//
+// 
+// 数组内已种好的花不会违反种植规则。 
+// 输入的数组长度范围为 [1, 20000]。 
+// n 是非负整数，且不会超过输入数组的大小。 
+// 
+// Related Topics 数组
+
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        int cnt = 0;//实际可以种下的数量
+        for (int i = 0; i < len && cnt < n; i++) {
+            if (flowerbed[i] == 1) {
+                continue;
+            }
+            //当前是第一棵吗？
+            int pre = i == 0 ? 0 : flowerbed[i - 1];
+            //当前是最后一棵吗？
+            int next = i == len - 1 ? 0 : flowerbed[i + 1];
+            //当前位置的前一棵和后一棵都为0
+            if (pre == 0 && next == 0) {
+                cnt++;
+                flowerbed[i] = 1;
+            }
+        }
+        return cnt >= n;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
